703aa11ea5705b14869247ea8efc3c7bceb79d42
Nathalie <nathaliemekahal@gmail.com>
2020-07-03 15:12:10 +0200

    df

--- src/server.js
+++ src/server.js
@@ -14,22 +14,25 @@ const {
 const server=express()
 
 const port = process.env.PORT
-server.use(cors());
-server.use(express.json())
-const whitelist =
-  process.env.NODE_ENV === "production"
-    ? [process.env.FE_URL]
-    : ["http://localhost:3000", "http://localhost:3002"]
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-}
+server.use(express.json())
+// const whitelist =
+//   process.env.NODE_ENV === "production"
+//     ? [process.env.FE_URL]:console.log('problem')
+//     
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+// }
+
+server.use(cors())
+
 server.use('/media',mediaRouter)
 server.use('/reviews',reviewsRouter)
 server.use(badRequestHandler)
